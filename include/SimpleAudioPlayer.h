39dcd626db2a9aa7291161daab7280748c4c6ef7
Christian Tronhjem <ctronhjem@gmail.com>
2024-12-01 14:46:52 +0100

    cleanups

--- include/SimpleAudioPlayer.h
+++ include/SimpleAudioPlayer.h
@@ -1,5 +1,7 @@
+#pragma once
 
 #include "portaudio.h"
+#define SAMPLE_RATE (44100)
 
 typedef struct
 {
@@ -7,51 +9,47 @@ typedef struct
     float right_phase;
 } paTestData;
 
-
-#define SAMPLE_RATE (44100)
-
 class SimpleAudioPlayer
 {
-
-/* This routine will be called by the PortAudio engine when audio is needed.
- * It may called at interrupt level on some machines so don't do anything
- * that could mess up the system like calling malloc() or free().
- */
-static int patestCallback(const void *inputBuffer, void *outputBuffer,
-                          unsigned long framesPerBuffer,
-                          const PaStreamCallbackTimeInfo *timeInfo,
-                          PaStreamCallbackFlags statusFlags,
-                          void *userData)
-{
-    /* Cast data passed through stream to our structure. */
-    paTestData* data = (paTestData *)userData;
-    float* out = (float *)outputBuffer;
-    unsigned int i;
-    (void)inputBuffer; /* Prevent unused variable warning. */
-
-
-    for (i = 0; i < framesPerBuffer; i++)
-    {
-        *(out++) = data->left_phase * 0.2f;  /* left */
-        *(out++) = data->right_phase * 0.2f; /* right */
-        /* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
-        data->left_phase += 0.01f;
-        /* When signal reaches top, drop back down. */
-        if (data->left_phase >= 1.0f)
-            data->left_phase -= 2.0f;
-        /* higher pitch so we can distinguish left and right. */
-        data->right_phase += 0.03f;
-        if (data->right_phase >= 1.0f)
-            data->right_phase -= 2.0f;
-    }
-    return 0;
-}
-
 public:
     void Run();
     void Stop();
 
     paTestData data;
-    PaStream *stream;
+    PaStream* stream;
     PaError err;
+
+private:
+    // Callback function for audio stream
+    static int patestCallback(const void *inputBuffer, void *outputBuffer,
+                            unsigned long framesPerBuffer,
+                            const PaStreamCallbackTimeInfo *timeInfo,
+                            PaStreamCallbackFlags statusFlags,
+                            void *userData)
+    {
+        /* Cast data passed through stream to our structure. */
+        paTestData* data = (paTestData *)userData;
+        float* out = (float *)outputBuffer;
+        unsigned int i;
+        (void)inputBuffer; /* Prevent unused variable warning. */
+
+
+        for (i = 0; i < framesPerBuffer; i++)
+        {
+            // Some sine wave trickery. 
+
+            *(out++) = data->left_phase * 0.2f;  /* left */
+            *(out++) = data->right_phase * 0.2f; /* right */
+            /* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
+            data->left_phase += 0.01f;
+            /* When signal reaches top, drop back down. */
+            if (data->left_phase >= 1.0f)
+                data->left_phase -= 2.0f;
+            /* higher pitch so we can distinguish left and right. */
+            data->right_phase += 0.03f;
+            if (data->right_phase >= 1.0f)
+                data->right_phase -= 2.0f;
+        }
+        return 0;
+    }
 };
--- src/SimpleAudioPlayer.cpp
+++ src/SimpleAudioPlayer.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "SimpleAudioPlayer.h"
 
-
 void SimpleAudioPlayer::Run()
 {
     err = Pa_Initialize();
@@ -12,31 +11,27 @@ void SimpleAudioPlayer::Run()
         return;
     }
 
-    /* Open an audio I/O stream. */
+    // Open an audio I/O stream
     err = Pa_OpenDefaultStream(&stream,
-                               0,         /* no input channels */
-                               2,         /* stereo output */
-                               paFloat32, /* 32 bit floating point output */
-                               SAMPLE_RATE,
-                               256,            /* frames per buffer, i.e. the number
-                                                      of sample frames that PortAudio will
-                                                      request from the callback. Many apps
-                                                      may want to use
-                                                      paFramesPerBufferUnspecified, which
-                                                      tells PortAudio to pick the best,
-                                                      possibly changing, buffer size.*/
-                               patestCallback, /* this is your callback function */
-                               &data);         /*This is a pointer that will be passed to
-                                                         your callback*/
+        0,              // no input channels
+        2,              // Channels
+        paFloat32,      // 32 bit floating point output
+        SAMPLE_RATE,    // Sample Rate
+        256,            // buffer size
+        patestCallback, // Callback function
+        &data);         // This is a pointer that will be passed to the callback
+                                    
     if (err != paNoError)
     {
         std::cout << "failed to open stream" << std::endl;
         return;
     }
 
-    // Start wait stop.
     err = Pa_StartStream( stream );
-
+    if (err != paNoError)
+    {
+        std::cout << "failed to start stream" << std::endl;
+    }
 }
 
 void SimpleAudioPlayer::Stop()
